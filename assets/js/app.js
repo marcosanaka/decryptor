3c9be7ad56758c47b3a45b855712bb3f38f48468
Marcos <marcos_anaka@hotmail.com>
2024-02-18 23:53:14 -0300

    Incluso arquivo app.js

---
+++ assets/js/app.js
@@ -0,0 +1,36 @@
+//Variaveis
+
+function exibirTextoNaTela(mensagem) {
+    let textoResultado = document.querySelector('#textoResultado');
+    textoResultado.innerHTML = mensagem ;
+}
+
+function codificarTexto() {
+    let mensagem = document.querySelector('#textoPrincipal').value;
+    x=0;
+    let mensagemcod = '';
+    while(mensagem[x])  {
+        let ascii = mensagem[x].charCodeAt();
+        ascii+=3;
+        mensagemcod = mensagemcod + String.fromCharCode(ascii);
+        x++;
+    } 
+console.log(mensagemcod);
+exibirTextoNaTela(mensagemcod);
+
+}
+
+function decodificarTexto() {
+    let mensagem = document.querySelector('#textoPrincipal').value;
+    x=0;
+    let mensagemcod = '';
+    while(mensagem[x])  {
+        let ascii = mensagem[x].charCodeAt();
+        ascii-=3;
+        mensagemcod = mensagemcod + String.fromCharCode(ascii);
+        x++;
+    } 
+console.log(mensagemcod);
+exibirTextoNaTela(mensagemcod);
+
+}
